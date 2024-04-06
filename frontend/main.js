4c7baeb8a4a429ea2aefc2efdef49f47c1f09c47
Nurmx7292 <raxmet2004@mail.ru>
2024-04-06 22:54:12 +0200

    Modal

--- frontend/main.js
+++ frontend/main.js
@@ -1,1 +1,6 @@
-
+let modal = document.querySelector(".modal")
+let overlay = document.querySelector(".overlay")
+function showInfo(task,bool){
+    bool=="block" ? modal.style.display = "flex" : modal.style.display = "none";
+    overlay.style.display = bool;
+}
